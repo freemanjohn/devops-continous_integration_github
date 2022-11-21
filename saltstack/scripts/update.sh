60bac92d498e0c831c405f46f9a09a44dd715e67
John Freeman <johnfreemangit@gmail.com>
2022-11-20 19:08:49 -0600

    adding saltstack workflow

--- saltstack/scripts/update.sh
+++ saltstack/scripts/update.sh
@@ -1,7 +1,7 @@
-if [ "${{ github.event.repository.name }}" =~ "salt_states" ]; then
+if [ "$CI_PROJECT_NAME" =~ "salt_states" ]; then
     salt-run cache.clear_git_lock gitfs type=update
     salt-run fileserver.update -l debug
-elif [ "${{ github.event.repository.name }}" =~ "salt_pillar" ]; then
+elif [ "$CI_PROJECT_NAME" =~ "salt_pillar" ]; then
     salt-run git_pillar.update -l debug
 else
     echo "+ Unkonwn project type executed"
